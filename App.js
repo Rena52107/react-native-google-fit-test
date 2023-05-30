b87964cb6191fe7eb40bbd428871b645653bc831
Rena Sakata <rena.sakata.cn@gmail.com>
2023-05-29 22:23:19 -0700

    temp test code

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import GoogleFit, { Scopes } from 'react-native-google-fit';
 
 export default function App() {
+  const [stepData, setStepData] = useState(0);
+
+  const options = {
+    scopes: [Scopes.FITNESS_ACTIVITY_READ, Scopes.FITNESS_ACTIVITY_WRITE],
+  };
+
+  useEffect(() => {
+    const checkAuthorization = async () => {
+      try {
+        await GoogleFit.checkIsAuthorized();
+        console.log(GoogleFit.isAuthorized);
+      } catch (err) {
+        console.log('Authorize failed: ', err.message);
+      }
+    };
+
+    checkAuthorization();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Test</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
